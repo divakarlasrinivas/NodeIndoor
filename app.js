963725d826683907f7a486be147e5b2f2bd5b646
Srinivas Divakarla <divakarlasrinivas@gmail.com>
2025-01-10 20:15:04 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,96 +1,96 @@
 const express = require('express');
 const path = require('path');
 const indexRouter = require('./routes/index');
+const nocache = require('nocache');
+const http = require('http');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-const nocache = require('nocache')
-const http = require('http')
-const cors = require('cors')
-var bodyParser = require('body-parser');
+// Create Express app and HTTP server
+const app = express();
+const server = http.Server(app);
+const port = process.env.PORT || 3000; // Use Railway-assigned PORT or default to 3000
 
-let app = express()
-let server = http.Server(app)
-let port = ${PORT}
+// Middleware setup
+app.use(express.static(path.join(__dirname, 'public'))); // Serve static files from 'public'
+app.use(nocache());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(__dirname))
-app.use(nocache())
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-
-// Return app.html page by default
-app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/public/html/app.html')
+// Default route to serve app.html
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public/html/app.html'));
 });
 
 // Post request to get the exit path
-app.post('/getPath', function(req, res){
-	let mapData = req.body.mapDataIn
-  let startPoint = req.body.startPointIn
-  let endPoint = req.body.endPointIn
+app.post('/getPath', function (req, res) {
+  const mapData = req.body.mapDataIn;
+  const startPoint = req.body.startPointIn;
+  const endPoint = req.body.endPointIn;
 
-	console.log(req.body)
+  console.log(req.body);
 
-	let possiblePaths = []
+  let possiblePaths = [];
+  let response = { exitPath: [] };
 
-	let response = { exitPath: []}
+  if (
+    mapData["data"].hasOwnProperty(startPoint) &&
+    mapData["data"].hasOwnProperty(endPoint) &&
+    mapData["size"] > 0
+  ) {
+    findExitPath(mapData, startPoint, endPoint, startPoint, [], possiblePaths);
 
-	if(mapData["data"].hasOwnProperty(startPoint) && mapData["data"].hasOwnProperty(endPoint) && mapData["size"] > 0) {
-		findExitPath(mapData, startPoint, endPoint, startPoint, [], possiblePaths)
+    const optimalPath = findOptimalPath(possiblePaths, mapData);
 
-		let optimalPath = findOptimalPath(possiblePaths, mapData)
+    response = {
+      exitPath: optimalPath
+    };
+  }
 
-	  response = {
-		  exitPath : optimalPath
-		}
-	}
-
-	res.send(response)
-})
+  res.send(response);
+});
 
 // Function to find all possible exit paths
 function findExitPath(mapDataIn, startVertex, endVertex, curVertex, path, possiblePathsIn) {
-  const tempPath = [...path]
+  const tempPath = [...path];
 
-  let curMap = mapDataIn
+  if (curVertex === endVertex) {
+    tempPath.push(curVertex);
+    possiblePathsIn.push(tempPath);
+  } else if (tempPath.length <= mapDataIn["size"] && !tempPath.includes(curVertex)) {
+    tempPath.push(curVertex);
 
-  if(curVertex === endVertex) {
-    tempPath.push(curVertex)
-    possiblePathsIn.push(tempPath)
-  }
-  else if(tempPath.length <= mapDataIn["size"] && !tempPath.includes(curVertex)) {
-    tempPath.push(curVertex)
+    const nextArr = mapDataIn.data[curVertex]["next"];
 
-    let nextArr = curMap.data[curVertex]["next"]
-
-    for(let i = 0; i < nextArr.length; i++) {
-      let nextVertex = nextArr[i]
-      let tempExitPath = findExitPath(mapDataIn, startVertex,
-        endVertex, nextVertex, tempPath, possiblePathsIn)
+    for (let i = 0; i < nextArr.length; i++) {
+      const nextVertex = nextArr[i];
+      findExitPath(mapDataIn, startVertex, endVertex, nextVertex, tempPath, possiblePathsIn);
     }
   }
 }
 
 // Function to return the best exit path from all possible paths
 function findOptimalPath(possiblePathsIn, mapDataIn) {
-  let minDistance = mapDataIn["size"] + 1
-  let bestPathIndex = -1
+  let minDistance = mapDataIn["size"] + 1;
+  let bestPathIndex = -1;
 
-  for(let i = 0; i < possiblePathsIn.length; i++) {
-    if(possiblePathsIn[i].length <= minDistance) {
-      minDistance = possiblePathsIn[i].length
-      bestPathIndex = i
+  for (let i = 0; i < possiblePathsIn.length; i++) {
+    if (possiblePathsIn[i].length <= minDistance) {
+      minDistance = possiblePathsIn[i].length;
+      bestPathIndex = i;
     }
   }
 
-  return possiblePathsIn[bestPathIndex]
+  return possiblePathsIn[bestPathIndex];
 }
 
-// Return app.html page by default
-app.get('*', function(req, res) {
-	res.sendFile(__dirname + '/public/html/app.html')
-})
+// Fallback route for undefined paths
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public/html/app.html'));
+});
 
-// Listen on port
-server.listen(port, function() {
-  console.log('listening on:' + port)
-})
+// Start server
+server.listen(port, function () {
+  console.log('Server listening on port: ' + port);
+});
